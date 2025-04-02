ab685d5db211d8c436c03a6a39bcc9e002fd8365
AngeloZorzi <astro.drg666@gmail.com>
2025-04-02 13:48:00 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import MyNav from './components/MyNav'
-import MyFooter from './components/MyFooter'
-import Welcome from './components/Welcome'
-import AllTheBooks from './components/AllTheBooks'
-import { Container } from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MyNav from "./components/MyNav";
+import MyFooter from "./components/MyFooter";
+import Welcome from "./components/Welcome";
+
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
@@ -12,11 +12,10 @@ function App() {
       <MyNav />
       <Container>
         <Welcome />
-        <AllTheBooks />
       </Container>
       <MyFooter />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
