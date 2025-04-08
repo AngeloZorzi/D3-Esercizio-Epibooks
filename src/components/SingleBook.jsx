de55b75adfe5d91db51d68ea8882b79fd081980e
AngeloZorzi <astro.drg666@gmail.com>
2025-04-08 13:23:13 +0200

    upd

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,33 +1,30 @@
-import { Component } from "react";
 import { Card } from "react-bootstrap";
 
-class SingleBook extends Component {
-  handleClick = () => {
-    this.props.onSelect(this.props.book.asin);
+const SingleBook = function (props) {
+  const handleClick = () => {
+    props.onSelect(props.book.asin);
   };
 
-  render() {
-    const { book, isSelected } = this.props;
+  const { book, isSelected } = props;
 
-    return (
-      <Card
-        className="book-cover d-flex flex-column"
-        style={{
-          border: isSelected ? "2px solid red" : "1px solid gray",
-        }}
-      >
-        <Card.Img
-          variant="top"
-          src={book.img}
-          alt={book.title}
-          onClick={this.handleClick}
-        />
-        <Card.Body>
-          <Card.Title className="text-center">{book.title}</Card.Title>
-        </Card.Body>
-      </Card>
-    );
-  }
-}
+  return (
+    <Card
+      className="book-cover d-flex flex-column"
+      style={{
+        border: isSelected ? "2px solid red" : "1px solid gray",
+      }}
+    >
+      <Card.Img
+        variant="top"
+        src={book.img}
+        alt={book.title}
+        onClick={handleClick()}
+      />
+      <Card.Body>
+        <Card.Title className="text-center">{book.title}</Card.Title>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default SingleBook;
